2cad526ae24ec8f88d4a6cc13498fbcd36f2c709
Hoang Sang <sangdlh95@gmail.com>
2017-09-15 14:05:31 +0700

    broadcast emit

--- server/server.js
+++ server/server.js
@@ -13,16 +13,43 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connect.');
+    /* 
+        socket.emit: sends the message to sender-client only
+
+        io.emit: sendings to all clients, include sender
+
+        io.sockets.emit will send to all the clients
+
+        socket.broadcast.emit will send the message to all the other clients except the newly created connection
+    */
+
 
     socket.emit('newMessage', {
         from: 'Admin',
-        text: 'Hey, you awesome.',
-        createdAt: 123,
+        text: 'Welcome to the chat app.',
+        createAt: new Date().getTime(),
     });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage)
-    })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined.',
+        createAt: new Date().getTime(),
+    });
+
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime(),
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime(),
+        // })
+    });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected.');
@@ -30,5 +57,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+    console.log(`Server is up on ${port}`);
 });

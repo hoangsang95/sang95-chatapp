e42d9efd49c32101b6e1fe169f88ab99c11bbf24
Hoang Sang <sangdlh95@gmail.com>
2017-09-18 18:47:05 +0700

    autocomplete

--- public/js/index.js
+++ public/js/index.js
@@ -10,9 +10,9 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createAt).locale('vi').format('HH:mm');
-    var li = jQuery('<li></li>');
     var str = message.text.split(" ");
     var arrResult = [];
+    var checkLink = false;
     for (var i = 0; i < str.length; i++) {
         if (validUrl(str[i])) {
             str[i] = `<a target="_blank" href="${str[i]}">${str[i]}</a>`;
@@ -21,38 +21,78 @@ socket.on('newMessage', function (message) {
     };
     arrResult.forEach(item => {
         if (validUrl(item)) {
-            return li.html(`${message.from} ${formattedTime} : ` + arrResult.join(' '));
+            return checkLink = true;
         }
-        li.text(`${message.from} ${formattedTime}: ` + arrResult.join(' '));
     });
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: arrResult.join(' '),
+        createAt: formattedTime,
+        checkLink
+    });
+    jQuery('#messages').append(html);
+    scrollBottom();
+    // var li = jQuery('<li></li>');
+    // var str = message.text.split(" ");
+    // var arrResult = [];
+    // for (var i = 0; i < str.length; i++) {
+    //     if (validUrl(str[i])) {
+    //         str[i] = `<a target="_blank" href="${str[i]}">${str[i]}</a>`;
+    //     }
+    //     arrResult.push(str[i]);
+    // };
+    // arrResult.forEach(item => {
+    //     if (validUrl(item)) {
+    //         return li.html(`${message.from} ${formattedTime} : ` + arrResult.join(' '));
+    //     }
+    //     li.text(`${message.from} ${formattedTime}: ` + arrResult.join(' '));
+    // });
+    // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createAt).locale('vi').format('HH:mm');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime} : `);
-    var a = jQuery('<a target="_blank">My current location</a>');
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-location-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+    scrollBottom();
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime} : `);
+    // var a = jQuery('<a target="_blank">My current location</a>');
+    // a.attr('href', message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li);
 });
 
 socket.on('user image', function (message) {
     var formattedTime = moment(message.createAt).locale('vi').format('HH:mm');
-    var li = jQuery('<li></li>');
-    var p = jQuery('<p></p>');
-    li.text(`User ${formattedTime} : `).append('<br><br>');
-    var img = jQuery('<img/>');
-    img.attr('src', message.imageData);
-    li.append(img);
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-image-template').html();
+    var html = Mustache.render(template, {
+        from: 'User',
+        text: message.imageData,
+        createAt: formattedTime,
+
+    });
+    jQuery('#messages').append(html);
+    scrollBottom();
+    // var li = jQuery('<li></li>');
+    // var p = jQuery('<p></p>');
+    // li.text(`User ${formattedTime} : `).append('<br><br>');
+    // var img = jQuery('<img/>');
+    // img.attr('src', message.imageData);
+    // li.append(img);
+    // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     var messageInput = jQuery('[name=message]');
-    if(messageInput.val().length === 0){
+    if (messageInput.val().length === 0) {
         return false;
     }
     socket.emit('createMessage', {
@@ -91,11 +131,10 @@ locationButton.on('click', function () {
 });
 
 jQuery('#imageFile').on('change', function (e) {
-    var file = e.originalEvent.target.files[0],
-        reader = new FileReader();
-
+    var file = e.originalEvent.target.files[0];
+    var reader = new FileReader();
+    console.log(file);
     reader.onload = function (evt) {
-        console.log(evt);
         var jsonObject = {
             'imageData': evt.target.result
         }
@@ -105,10 +144,30 @@ jQuery('#imageFile').on('change', function (e) {
     };
 
     reader.readAsDataURL(file);
+     $(this).val("");
+});
+
+jQuery('#send-image').on('click', function () {
+    jQuery('#imageFile').click();
 });
 
 function validUrl(s) {
     var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
     return regexp.test(s);
-}
+};
+
+function scrollBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
 
--- server/server.js
+++ server/server.js
@@ -29,12 +29,12 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
     });
 
     socket.on('user image', (message) => {
+        console.log('create image');
         io.emit('user image', message);
     });
 
